04d0fa94295ebdbb8edfabb20f52de4b28d0844b
perinze <perinzefaper@foxmail.com>
2022-07-21 11:00:48 +0800

    721

--- init.el
+++ init.el
@@ -1,48 +1,6 @@
-;(require 'ggtags)
-;;(add-hook 'c-mode-common-hook
-;;          (lambda ()
-;;            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-;;              (ggtags-mode 1))))
-
-;;(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
-;;(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
-;;(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
-;;(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
-;;(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
-;;(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
-
-;;(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
-
-;;(setq-local imenu-create-index-function #'ggtags-build-imenu-index)
-
-
-;;(setq speedbar-show-unknown-files t)
-
-
-
-;; function-args
-;; (require 'function-args)
-;; (fa-config-default)
-;; (define-key c-mode-map  [(tab)] 'company-complete)
-;; (define-key c++-mode-map  [(tab)] 'company-complete)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 (require 'package)
 (add-to-list 'package-archives
-         '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
 
@@ -85,4 +43,47 @@
 (add-to-list 'company-backends 'company-c-headers)
 
 
-(display-line-numbers-mode 1)
+(display-line-numbers-mode)
+
+(load-theme 'zenburn t)
+
+;(require 'ggtags)
+;;(add-hook 'c-mode-common-hook
+;;          (lambda ()
+;;            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+;;              (ggtags-mode 1))))
+
+;;(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+;;(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+;;(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+;;(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+;;(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+;;(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+
+;;(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+
+;;(setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+
+
+;;(setq speedbar-show-unknown-files t)
+
+
+
+;; function-args
+;; (require 'function-args)
+;; (fa-config-default)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(zenburn-theme rust-mode magit zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
